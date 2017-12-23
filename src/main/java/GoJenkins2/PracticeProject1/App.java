c0bdc6609b657776438198998be1ae0b2266213a
pavanisura <pavani.sura267@gmail.com>
2017-12-23 14:26:25 +0530

    this id for practice

---
+++ src/main/java/GoJenkins2/PracticeProject1/App.java
@@ -0,0 +1,13 @@
+package GoJenkins2.PracticeProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/GoJenkins2/PracticeProject1/WhileLoop.java
@@ -0,0 +1,16 @@
+package GoJenkins2.PracticeProject1;
+
+class WhileLoop
+{
+    public static void main(String args[])
+    {
+         int i=10;
+         while(i>1){
+              System.out.println(i);
+              i--;
+         }
+    }
+}
+
+
+
